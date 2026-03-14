c8f7e79fc4e65e9eaa37076aa880c67eba7520b1
willa <willaevangelista.pro@gmail.com>
2026-03-14 16:29:56 -0300

    feat: add the solution for the exercise 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if ((num1 < 0) ||
+        (num2 < 0) ||
+        (Number.isInteger(num1) === false) ||
+        (Number.isInteger(num2) === false)) {
+
+        return 'ERROR';
+    } 
+
+    let bigger = num1 > num2 ? num1 : num2;
+    let smaller = num1 > num2 ? num2 : num1;
+
+    let result = 0;
+
+    for (let i = smaller; i <= bigger; i++) {
+        result += i;
+    }
+   
+
+    return result;
+    
 };
 
 // Do not edit below this line
