3adf012905f7235d226455e92e71feeee0f76cbf
willa <willaevangelista.pro@gmail.com>
2026-03-14 19:35:10 -0300

    feat: add the solution for the exercise 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(...number) {
+  let result = 0;
+  for (let i = 0; i < number.length; i++) {
+    result += number[i];
+  }
+  return Number(result);
 };
 
-const subtract = function() {
-	
+const subtract = function(...number) {
+  let result = number[0];
+  for (let i = 1; i < number.length; i++) {
+    result -= number[i];
+  }
+  return Number(result);
 };
 
-const sum = function() {
-	
+const sum = function(number) {
+  let result = 0;
+  for (let i = 0; i < number.length; i++) {
+    result += number[i];
+  }
+  return Number(result);
 };
 
-const multiply = function() {
-
+const multiply = function(number) {
+  let result = 1;
+  for (let i = 0; i < number.length; i++) {
+    result *= number[i];
+  }
+  return Number(result);
 };
 
-const power = function() {
-	
+const power = function(...number) {
+  let result = number[0];
+  
+  result **= number[1];
+  
+  return Number(result);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+  for (let i = 0; i < number; i++) {
+    result *= (number-i);
+  }
+	return Number(result);
 };
 
 // Do not edit below this line
