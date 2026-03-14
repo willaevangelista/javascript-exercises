75cf06e035a2a381b0f0fc81d6f849a101101cd7
willa <willaevangelista.pro@gmail.com>
2026-03-14 17:15:43 -0300

    feat: add the solution for the exercise 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let result = (fahrenheit - 32) * (5 / 9);
+  return Number(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let result = (celsius * (9 / 5)) + 32;
+  return Number(result.toFixed(1));
 };
 
 // Do not edit below this line
