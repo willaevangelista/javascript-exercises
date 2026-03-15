b75097770e7a5bce8e669c31cd7555d1a2595bc9
willa <willaevangelista.pro@gmail.com>
2026-03-14 21:55:17 -0300

    feat: add the solution for the exercise 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    let originalString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    
+    let stringSplit = originalString.split("");
+
+    let reverseString = ((stringSplit.reverse()).join("")).toLowerCase();
+
+    return originalString === reverseString ? true : false;
 };
 
 // Do not edit below this line
